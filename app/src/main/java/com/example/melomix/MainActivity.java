274ab9690c9790a06aec67a96070d68a5961abeb
mazharul-15 <mazharulislamhstu15@gmail.com>
2024-05-06 09:40:34 +0600

    run time permission features added

--- app/src/main/java/com/example/melomix/MainActivity.java
+++ app/src/main/java/com/example/melomix/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.melomix;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.widget.ArrayAdapter;
@@ -21,6 +25,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int EXTERNAL_STORAGE_REQUEST_CODE = 100;
     ListView listView;
     String[] items;
 
@@ -30,12 +35,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.listView);
-        runTimePermission();
+        //runTimePermission();
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    EXTERNAL_STORAGE_REQUEST_CODE);
+        }else {
+
+            displaySongs();
+        }
         //displaySongs();
 
     }
 
-    public void runTimePermission() {
+    /*public void runTimePermission() {
         Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
                 .withListener(new PermissionListener() {
                     @Override
@@ -53,6 +68,21 @@ public class MainActivity extends AppCompatActivity {
                         permissionToken.continuePermissionRequest();
                     }
                 }).check();
+    }*/
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == EXTERNAL_STORAGE_REQUEST_CODE) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                displaySongs();
+            }else {
+                Toast.makeText(this, "Permission is not granted", Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(this, "Permission won't Granted", Toast.LENGTH_SHORT).show();
+        }
     }
 
     // Accessing File from External Storage
